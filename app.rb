5b7d3065f3bbae55b92825512c71b5d5e45d3853
vanina123 <nanavanina834@gmail>
2023-07-01 18:36:29 +0100

    modify both app and main.rb file

--- app.rb
+++ app.rb
@@ -4,7 +4,6 @@ require_relative 'student'
 require_relative 'teacher'
 require_relative 'book'
 require_relative 'rental'
-
 class App
   def initialize
     @people = []
@@ -13,80 +12,81 @@ class App
 
   def list_all_books
     @books.each do |book|
-      puts "Title: #{book.title}, Author: #{book.author}"
+      puts "#{book.title} by #{book.author} on #{book.rental.count} rentals"
     end
   end
 
   def list_all_people
     @people.each do |person|
-      puts "ID: #{person.id}, Name: #{person.name}"
+      puts "[#{person.class}] Name: #{person.name}, ID: #{person.id}, , Age: #{person.age}"
     end
   end
 
-  def create_a_teacher
-    puts 'enter your name'
+  def create_person
+    print 'Do you want to create a student (1) or a teacher (2)? '
+    person_type = gets.chomp.to_i
+    print 'Name:'
     name = gets.chomp
-    puts 'enter your age'
+    print 'Age:'
     age = gets.chomp.to_i
-    puts 'enter your specialization'
-    specialization = gets.chomp
-    teacher = Teacher.new(name, age, specialization)
-    @people << teacher
-    puts 'Teacher created successfully.'
-  end
-
-  def create_a_student
-    puts 'enter your name'
-    name = gets.chomp
-    puts 'enter your age'
-    age = gets.chomp.to_i
-    puts 'what is the parent_permission? (Y/N)'
-    parent_permission = gets.chomp
-
-    student = Student.new(name, age, parent_permission)
-    @people << student
-    puts 'Student created successfully.'
+    if person_type == 1
+      print 'Whats the parent permission? (y/n) '
+      parent_permission = gets.chomp == 'y'
+      @people << Student.new(name, age, parent_permission)
+      puts 'Student created!'
+    elsif person_type == 2
+      print 'Specialization:'
+      specialization = gets.chomp
+      @people << Teacher.new(name, age, specialization)
+      puts 'Teacher created!'
+    else
+      puts 'Person not created!'
+    end
   end
 
   def create_a_book
-    puts 'enter the title of the book'
+    print 'Title:'
     title = gets.chomp
-    puts 'enter the author'
+    print 'Author:'
     author = gets.chomp
-
     book = Book.new(title, author)
     @books << book
     puts 'Book created successfully.'
   end
 
   def create_a_rental
-    puts 'Choose a book from the list by number'
+    puts 'Choose a book from the list by number: (not Id)'
     @books.each_with_index do |book, index|
-      puts "#{index} - #{book.title} by #{book.author}"
+      puts "#{index}) #{book.title} by #{book.author}"
     end
     book_index = gets.chomp.to_i
     book = @books[book_index]
-    puts 'Choose a person from the list by number:'
+    puts 'Choose a person from the list by number: (not Id)'
     @people.each_with_index do |person, index|
-      puts "#{index} - #{person.name} - #{person.age} - #{person.class}"
+      puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.age}, Age: #{person.age} "
     end
     person_index = gets.chomp.to_i
     person = @people[person_index]
-    puts 'Date: '
+    puts 'Date in format yyyy/mm/dd:'
     date = gets.chomp
     Rental.new(date, book, person)
     puts 'Rental created!'
   end
 
-  def list_rentals_for_a_person
-    puts 'Choose a person from the list by number'
-    @people.each_with_index do |person, index|
-      puts "#{index} - #{person.name}"
+  def list_all_rentals_for_a_given_person_id
+    @people.each do |person|
+      puts "ID: #{person.id}, Name: #{person.name}"
     end
-    person_index = gets.chomp.to_i
-    person = @people[person_index]
+    print 'ID of person: '
+    person_id = gets.chomp.to_i
+    puts 'Rentals: '
+
+    person = @people.each do |p|
+      break p if p.id == person_id
+    end
+
     person.rental.each do |rental|
-      puts "Book: #{rental.book.title} - Date: #{rental.date}"
+      puts "Date: #{rental.date}, Book: \"#{rental.book.title}\" by #{rental.book.author}"
     end
   end
 
--- main.rb
+++ main.rb
@@ -3,12 +3,11 @@ def display
   {
     1 => 'list_all_books',
     2 => 'list_all_people',
-    3 => 'create_a_teacher',
-    4 => 'create_a_student',
-    5 => 'create_a_book',
-    6 => 'create_a_rental',
-    7 => 'list_rentals_for_a_person',
-    8 => 'quit'
+    3 => 'create_person',
+    4 => 'create_a_book',
+    5 => 'create_a_rental',
+    6 => 'list_all_rentals_for_a_given_person_id',
+    7 => 'quit'
   }
 end
 
