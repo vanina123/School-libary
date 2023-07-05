527d1383d22aef6bbc18e7003e2e0dbb840bbf3c
Benson Njuguna <njugunab655@gmail.com>
2023-07-05 20:16:17 +0300

    Add spec file for person

---
+++ spec/person_spec.rb
@@ -0,0 +1,35 @@
+require_relative '../person.rb'
+require_relative '../rental.rb'
+
+describe Person do
+  before :each do
+    @person = Person.new("John", 23, 543, true)
+  end
+
+  it "Should create a person of same instance" do
+    expect(@person).to be_instance_of(Person)
+  end
+
+  it "Should give the correct details" do
+    expect(@person.name).to eq("John")
+    expect(@person.age).to eq(23)
+    expect(@person.id).to eq(543)
+    expect(@person.parent_permission).to eq(true)
+  end
+
+  it "Should return trur for can use services" do
+    expect(@person.can_use_services?).to eq(true)
+  end
+
+  it "should be able to add a rental" do
+    person = instance_double(Person, name: "John Doe")  # Create a test double for Person
+    rental = instance_double(Rental)  # Create a test double for Rental
+    allow(Rental).to receive(:new).and_return(rental)  # Mock the Rental.new method
+    allow(rental).to receive(:date=)
+    allow(rental).to receive(:book=)
+    allow(rental).to receive(:person=)
+
+    expect(@person.add_rental("2023/12/12", person)).to eq(rental)
+    expect(@person.rental).to be_truthy
+  end
+end
